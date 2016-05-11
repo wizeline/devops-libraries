760722d240ac032297daa9e8336a1179cff98da6
Leon Garcia <leon@wizeservices.com>
2016-05-11 16:05:42 -0500

    Do not pull if TYPE equals DEVELOPMENT

--- gitfunctions.sh
+++ gitfunctions.sh
@@ -4,7 +4,9 @@
 if [ -d $REPO ]; then
     pushd $REPO
     git checkout $BRANCH || exit 1
-    git pull origin $BRANCH
+    if [[ $TYPE = "DEVELOPMENT" ]]; then
+        git pull origin $BRANCH
+    fi
 else
     git clone https://$GITHUB_TOKEN@github.com/$GIT_USER/$REPO.git || exit 1
     pushd $REPO
